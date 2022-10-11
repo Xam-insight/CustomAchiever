6e28ffb48e4b64938ffae94cf0b6627b8ed2b4b9
Xam <tportevin@gmail.com>
2022-10-11 17:36:15 +0200

    New parameter in CustAc_CompleteAchievement: soundAskedBySourceAddon.

--- CustomAchieverTab.lua
+++ CustomAchieverTab.lua
@@ -126,7 +126,7 @@ function CustAc_UpdateAchievement(id, parent, icon, points, name, description, l
 	end
 end
 
-function CustAc_CompleteAchievement(id, earnedBy, noNotif, forceNotif)
+function CustAc_CompleteAchievement(id, earnedBy, noNotif, forceNotif, soundAskedBySourceAddon)
 	if id and CustomAchieverData["Achievements"][id] then
 		local earnedByWithRealm = CustAc_playerCharacter()
 		if earnedBy then
@@ -145,7 +145,7 @@ function CustAc_CompleteAchievement(id, earnedBy, noNotif, forceNotif)
 
 		if forPlayerCharacter and (not alreadyEarned or forceNotif) and not noNotif then
 			local name = data["name_"..GetLocale()] or data["name_enUS"] or data["name_enGB"] or data["name_frFR"] or data["name_deDE"] or data["name_esES"] or data["name_esMX"] or data["name_itIT"] or data["name_koKR"] or data["name_ptBR"] or data["name_ruRU"] or data["name_zhCN"] or data["name_zhTW"]
-			EZBlizzUiPop_ToastFakeAchievementNew(CustomAchiever, name, 5208, false, 15, "Custom Achiever", function() CustAc_ShowAchievement(id) end, data["icon"])
+			EZBlizzUiPop_ToastFakeAchievementNew(CustomAchiever, name, 5208, soundAskedBySourceAddon and not CustomAchieverOptionsData["CustomAchieverSoundsDisabled"], 15, "Custom Achiever", function() CustAc_ShowAchievement(id) end, data["icon"])
 		end
 		CustAc_LoadAchievementsData()
 		if CustAc_AchievementFrameAchievements and CustAc_AchievementFrameAchievements:IsShown() then
