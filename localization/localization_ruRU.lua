bbe3c4ab9478d1f2c355a8a1091d17f56dba878a
Xam-insight <tportevin@gmail.com>
2025-02-23 10:34:45 +0100

    Russian localization by ZamestoTV.

---
+++ localization/localization_ruRU.lua
@@ -0,0 +1,58 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("CustomAchiever", "ruRU", false)
+
+if L then
+
+L["CUSTOMACHIEVER_WELCOME"] = "Введите /custac, чтобы показать Custom Achiever."
+
+L["SPACE_BEFORE_DOT"] = ""
+
+L["GENERAL_SECTION"] = "Общие настройки"
+L["ENABLE_SOUND"] = "Включить звуки Custom Achiever"
+L["ENABLE_SOUND_DESC"] = "Включает/отключает звуки Custom Achiever"
+L["ENABLE_ACHIEVEMENT_ANNOUNCE"] = "Включить объявление достижений"
+L["ENABLE_ACHIEVEMENT_ANNOUNCE_DESC"] = "Включает/отключает окно объявления достижений"
+L["ENABLE_MINIMAPBUTTON"] = "Кнопка на миникарте"
+L["ENABLE_MINIMAPBUTTON_DESC"] = "Показывает/скрывает кнопку на миникарте"
+L["WINDOW_SECTION"] = "Настройки окна Custom Achiever"
+L["MENUOPTIONS_TOOLTIP"] = "Настройки Custom Achiever"
+L["MENUOPTIONS_TOOLTIPDETAIL"] = "Изменить настройки Custom Achiever."
+L["LOGS_TOOLTIP"] = "Журнал"
+L["LOGS_TOOLTIPDETAIL"] = "Показывает/скрывает журнал."
+
+L["REFRESH_TOOLTIP"] = "Обновить"
+L["REFRESH_TOOLTIPDETAIL"] = "Отправить достижения этой категории цели|nи известным пользователям Custom Achiever."
+
+L["MENUCUSTAC_CATEGORY"] = "Категория"
+L["MENUCUSTAC_CATEGORIES"] = "Категории"
+L["MENUCUSTAC_NEWCATEGORY"] = "Создать категорию"
+L["MENUCUSTAC_ACHIEVEMENT"] = "Достижение"
+L["MENUCUSTAC_ACHIEVEMENTS"] = "Пользовательские достижения"
+L["MENUCUSTAC_NEW"] = "Создать новое достижение"
+L["MENUCUSTAC_DEFAULT_NAME"] = "Пользовательское достижение"
+L["MENUCUSTAC_ICON"] = "Иконка"
+L["MENUCUSTAC_POINTS"] = "Очки"
+L["MENUCUSTAC_CAT_CONFIRM_DELETION"] = "Вы уверены, что хотите навсегда удалить категорию |cff00ff00%s|r? Достижения этой категории будут перемещены в |cff00ff00%s|r."
+L["MENUCUSTAC_CONFIRM_DELETION"] = "Вы уверены, что хотите навсегда удалить пользовательское достижение |cff00ff00%s|r?"
+
+L["MENUCUSTAC_MOVE_ACHIEVEMENT"] = "Переместить выбранное достижение в эту категорию."
+L["MENUCUSTAC_MOVE_CATEGORY"] = "Переместить выбранную категорию в эту категорию."
+L["MENUCUSTAC_EXTRACT_CATEGORY"] = "Извлечь подкатегорию."
+
+L["MENUCUSTAC_AWARD"] = "Наградить"
+L["MENUCUSTAC_REVOKE"] = "Отозвать"
+
+L["LOGCUSTAC_AWARD"] = "Достижение |cff00ff00%s|r присуждено %s."
+L["LOGCUSTAC_REVOKE"] = "Достижение |cff00ff00%s|r отозвано у %s."
+
+L["SHARECUSTAC_WAIT"] = "Подождите %d |4минуту:минут; %d |4секунду:секунд; перед повторной отправкой достижений этому игроку."
+L["SHARECUSTAC_NOACKNOWLEDGMENT"] = "Похоже, у %s не установлен Custom Achiever или он не в сети."
+
+L["MENUOPTIONS_TOOLTIP"] = "Настройки Custom Achiever"
+L["MENUOPTIONS_TOOLTIPDETAIL"] = "Изменить настройки Custom Achiever."
+
+L["MINIMAP_TOOLTIP1"] = "ЛКМ, чтобы показать Custom Achiever."
+L["MINIMAP_TOOLTIP2"] = "ПКМ, чтобы открыть панель настроек."
+
+L["CUSTAC_HELPTIP_AWARD"] = "Присудить достижение вашей цели (себе, если цели нет).|n|nУ вашей цели должен быть установлен CustomAchiever."
+
+end
